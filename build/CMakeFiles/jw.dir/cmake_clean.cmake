95afdb9c472c6308ab7abf01cebd8ce8ce37cf5a
mazi <maximiliano.bernal@ug.uchile.cl>
2025-03-17 17:22:37 -0300

    cmake17/03

---
+++ build/CMakeFiles/jw.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jw.dir/jordanwigner.cc.o"
+  "CMakeFiles/jw.dir/jordanwigner.cc.o.d"
+  "CMakeFiles/jw.dir/jw.cc.o"
+  "CMakeFiles/jw.dir/jw.cc.o.d"
+  "jw"
+  "jw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/jw.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jw.
+# This may be replaced when dependencies are built.
